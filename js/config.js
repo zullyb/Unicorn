80c847224c8f234bcf32830db50b1524524a33e8
Zuleyma Baltazar <104181612+zullyb@users.noreply.github.com>
2023-12-03 16:42:02 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-east-1_57LBe2EnR',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '41uapo7fc6m0i5361taseoshhe',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             'us-east-1'          // e.g. us-east-2
+        userPoolId: 'us-east-1_57LBe2EnR',
+        userPoolClientId: '41uapo7fc6m0i5361taseoshhe',
+        region: 'us-east-1',
     },
     api: {
-        invokeUrl:          'https://ldhc6itvyf.execute-api.us-east-1.amazonaws.com/devol'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-        
+        invokeUrl: 'https://ldhc6itvyf.execute-api.us-east-1.amazonaws.com/dev', // Corrected the URL
     }
 };
+
